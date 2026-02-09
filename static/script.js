bee181a5090a6d9e5f1bc87dce350905c7a6f258
shravani-thorat <shravanithorat9@gmail.com>
2026-02-09 16:38:51 +0530

    scripting firebase connection

--- static/script.js
+++ static/script.js
@@ -1,6 +1,8 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js";
+
 function addMedicine() {
     const div = document.createElement("div");
-
     div.innerHTML = `
         <br>
         <label>Medicine</label>
@@ -12,74 +14,52 @@ function addMedicine() {
         <label>Days</label>
         <input type="number" name="days[]" placeholder="e.g. 30" min="1" required>
     `;
-
     document.getElementById("medicines").appendChild(div);
 }
 
-document.addEventListener("DOMContentLoaded",() => {
-    if("notification" in window) {
-        Notification.requestPermission();
-    }
-});
+// Firebase Config
+const firebaseConfig = {
+    apiKey: "AIzaSyC5xLeT44La7Cfz5w2l5NgF5MWVzh_gtyY",
+    authDomain: "rxremind-bb83e.firebaseapp.com",
+    projectId: "rxremind-bb83e",
+    messagingSenderId: "1098454881651",
+    appId: "1:1098454881651:web:fe4cf0689ab90bf10f6106"
+};
 
-function ShowReminderNotification(title, message) {
-    if (Notification.permission === "granted") {
-        new Notification(title, {
-            body: message,
-            icon: "/static/icon.png"
-        });
-    }
-}
-
-function checkReminders() {
-    fetch("/api/reminders")
-    .then(res => res.json())
-    .then(data => {
-        if(data.length > 0){
-            let r = data[0];
-            ShowReminderNotification(
-                "Medicine Refill Reminder",
-                `${r.name} - ${r.medicine} - Call customer`
-            );
-        }
-    });
-}
-
-setInterval(checkReminders, 60*60*1000);
-
+// Register Service Worker + Get Token
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/firebase-messaging-sw.js")
-    .then((registration) => {
+    navigator.serviceWorker.register("/firebase-messaging-sw.js")
+        .then((registration) => {
 
-      const app = initializeApp(firebaseConfig);
-      const messaging = getMessaging(app);
+            const app = initializeApp(firebaseConfig);
+            const messaging = getMessaging(app);
 
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
+            Notification.requestPermission().then((permission) => {
+                if (permission === "granted") {
 
-          getToken(messaging, {
-            vapidKey: "BG0KjYPTQWnUHl5ozG4xntepXG_53yc0RCVYLvEN_9_OuWotIUBoUcgBY7mgnLeZEA8oDorgrrDTRrehvBknu2A",
-            serviceWorkerRegistration: registration
-          }).then((currentToken) => {
+                    getToken(messaging, {
+                        vapidKey: "BG0KjYPTQWnUHl5ozG4xntepXG_53yc0RCVYLvEN_9_OuWotIUBoUcgBY7mgnLeZEA8oDorgrrDTRrehvBknu2A",
+                        serviceWorkerRegistration: registration
+                    }).then((currentToken) => {
 
-            if (currentToken) {
-              console.log("Token:", currentToken);
+                        if (currentToken) {
+                            console.log("Token:", currentToken);
 
-              fetch("/save-token", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ token: currentToken })
-              });
+                            fetch("/save-token", {
+                                method: "POST",
+                                headers: { "Content-Type": "application/json" },
+                                body: JSON.stringify({ token: currentToken })
+                            });
 
-            } else {
-              console.log("No token received.");
-            }
+                        } else {
+                            console.log("No token received.");
+                        }
 
-          }).catch((err) => {
-            console.error("Error getting token:", err);
-          });
-        }
-      });
+                    }).catch((err) => {
+                        console.error("Error getting token:", err);
+                    });
+                }
+            });
 
-    });
+        });
 }
